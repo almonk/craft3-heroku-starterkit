9bad3138924d276dd4e453494a18a1d85b267d29
Alasdair Monk <alasdair.monk@gmail.com>
2019-05-10 10:31:59 +0100

    Test

--- config/db.php
+++ config/db.php
@@ -1,21 +1,20 @@
 <?php
-
 /**
  * Database Configuration
  *
- * All of your system's database configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ * All of your system's database connection settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/DbConfig.php.
+ *
+ * @see craft\config\DbConfig
  */
 
-$url = getenv('JAWSDB_MARIA_URL');
-
-$dbparts = parse_url($url);
-
-return array(
-  'server' => $dbparts['host'],
-  'user' => $dbparts['user'],
-  'password' => $dbparts['pass'],
-  'database' => ltrim($dbparts['path'],'/'),
-  'tablePrefix' => 'craft',
-  'port' => $dbparts['port'],
-);
+return [
+    'driver' => getenv('DB_DRIVER'),
+    'server' => getenv('DB_SERVER'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'database' => getenv('DB_DATABASE'),
+    'schema' => getenv('DB_SCHEMA'),
+    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+    'port' => getenv('DB_PORT')
+];
