5cd7a25b54acf5da552c1c59521fbbdc9c11abfc
Alasdair Monk <alasdair.monk@gmail.com>
2019-05-10 10:37:35 +0100

    fix db

--- config/db.php
+++ config/db.php
@@ -7,14 +7,15 @@
  *
  * @see craft\config\DbConfig
  */
+$url = getenv('JAWSDB_MARIA_URL');
 
-return [
-    'driver' => getenv('DB_DRIVER'),
-    'server' => getenv('DB_SERVER'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
-    'schema' => getenv('DB_SCHEMA'),
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
-];
+$dbparts = parse_url($url);
+
+return array(
+  'server' => $dbparts['host'],
+  'user' => $dbparts['user'],
+  'password' => $dbparts['pass'],
+  'database' => ltrim($dbparts['path'],'/'),
+  'tablePrefix' => 'craft',
+  'port' => $dbparts['port'],
+);
